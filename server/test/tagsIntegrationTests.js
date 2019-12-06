fc9345638df8ef27d4a24487a01e7931cb0d4f73
Joseph cunningham <jham109@msn.com>
2019-12-05 23:53:36 -0800

    implemented create tag -POST and GET integration test

--- server/test/tagsIntegrationTests.js
+++ server/test/tagsIntegrationTests.js
@@ -29,4 +29,49 @@ describe("Tag Integration Tests: ", () => {
 
         });
     });
+    
+    describe("Create tag - POST and GET", () => {
+
+        var tag_name = "test";
+        const organization = "Test Org";
+        const token = "a61c2fa0-e977-4982-9871-071514b2bc92";
+
+        
+        before(async () => {
+            await db.executeQuery(`INSERT INTO tokens (token, organization, issued) VALUES ('${token}', '${organization}', CURRENT_TIMESTAMP());`);
+        });
+
+        beforeEach(() => {
+
+        });
+
+        it("POST tag/", (done) => {
+            chai.request(app)
+            .post('/api/tags/')
+            .send({name: tag_name})
+            .end((err, res) => {
+                res.should.have.status(201);
+                tags = res.body.tags.name;
+                done();
+            });
+
+        });
+        
+        it("GET tag/", (done) => {
+            chai.request(app)
+            .get(`/api/tags/`)
+            .end((err, res) => {
+                res.should.have.status(200);
+                assert.equal(res.body.tag.name, "test");
+                done();
+            });
+        });
+
+        after(() => {
+            db.executeQuery(`DELETE FROM tags`).then((results) => {});
+        });
+    });
 });
+
+
+
